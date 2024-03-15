49d292a1bb1dd5e3736a0c3efa72e793816e7300
Rushit16102004 <bodrarushit@gmail.com>
2024-03-15 16:33:30 +0530

    nav bar and css

---
+++ all.js
@@ -0,0 +1,8 @@
+function showSlidebar(){
+    const slidebar = document.querySelector('.slidebar');
+    slidebar.style.display = 'flex';
+}
+function hideSlidebar(){
+    const slidebar = document.querySelector('.slidebar');
+    slidebar.style.display = 'none';
+}
