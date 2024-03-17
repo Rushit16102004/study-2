72c22a9c6a0416b585d1b8d588d9a8cf409d0b1a
Rushit16102004 <bodrarushit@gmail.com>
2024-03-17 10:34:15 +0530

    nav html change

--- all.js
+++ all.js
@@ -6,3 +6,26 @@ function hideSlidebar(){
     const slidebar = document.querySelector('.slidebar');
     slidebar.style.display = 'none';
 }
+const slidebar = document.querySelector('.slidebar');
+slidebar.style.display = 'none';
+
+if (localStorage.getItem('displayPopup') !== '0') {
+    const openTime = Date.now();
+  
+    const totalVisitTime = +localStorage.getItem('totalVisitTime');
+    document.getElementById('container').innerHTML = totalVisitTime;
+  
+    setTimeout(() => {
+      alert('2 Minute Timeout');
+      localStorage.setItem('displayPopup', '0');
+      localStorage.removeItem('totalVisitTime');
+    }, 12e4 - totalVisitTime);
+  
+    window.addEventListener('beforeunload', () => {
+      if (localStorage.getItem('displayPopup') === '0') return;
+      localStorage.setItem(
+        'totalVisitTime',
+        totalVisitTime + Date.now() - openTime
+      );
+    });
+  }
