0a14e91191765e8a92cbc06e2643acbe81bcb1eb
Rushit16102004 <bodrarushit@gmail.com>
2024-03-26 11:18:35 +0530

    add Drop_down_item in nav bar

--- all.js
+++ all.js
@@ -14,19 +14,24 @@ function hideSlidebar(){
 const slidebar = document.querySelector('.slidebar');
 slidebar.style.display = 'none';
 
-function display_sub_Menu() {
-    const sub_Menu = document.querySelector('.sub_Menu');
-    sub_Menu.style.display = 'flex';
+//nav bar hide part
+function display_oldpaper_branch() {
+    const oldpaper_sub_Menu = document.querySelector('.oldpaper_sub_Menu');
+    oldpaper_sub_Menu.style.display = 'flex';
 }
+function display_studymaterial_branch() {
+    const studymaterial_branch_sub_Menu = document.querySelector('.studymaterial_branch_sub_Menu');
+    studymaterial_branch_sub_Menu.style.display = 'flex';
+}
+
 function hide_sub_Menu() {
-    const sub_Menu = document.querySelector('.sub_Menu');
-    sub_Menu.style.display = 'none';
+    const oldpaper_sub_Menu = document.querySelector('.oldpaper_sub_Menu');
+    oldpaper_sub_Menu.style.display = 'none';      
 }
 
 function sem_1and2(){
     const sem_1_and_2 = document.querySelector('.sem_1_and_2');
     sem_1_and_2.style.display = 'flex';
-    sem_1_and_2.style.backgroundcolor = black;
     const sem_3 = document.querySelector('.sem_3');
     sem_3.style.display = 'none';
     const sem_4= document.querySelector('.sem_4');
@@ -37,7 +42,21 @@ function sem_1and2(){
     sem_6.style.display = 'none';
     const sem_7 = document.querySelector('.sem_7');
     sem_7.style.display = 'none';
+    
+    const sem1_2 = document.querySelector('.sem1_2');
+    sem1_2.style.backgroundColor = 'black';
+    const sem3 = document.querySelector('.sem3');
+    sem3.style.backgroundColor = null;
+    const sem4= document.querySelector('.sem4');
+    sem4.style.backgroundColor = null;
+    const sem5 = document.querySelector('.sem5');
+    sem5.style.backgroundColor = null;
+    const sem6 = document.querySelector('.sem6');
+    sem6.style.backgroundColor = null;
+    const sem7 = document.querySelector('.sem7');
+    sem7.style.backgroundColor = null;
 }
+
 function sem_3(){
     const sem_1_and_2 = document.querySelector('.sem_1_and_2');
     sem_1_and_2.style.display = 'none';
@@ -51,6 +70,19 @@ function sem_3(){
     sem_6.style.display = 'none';
     const sem_7 = document.querySelector('.sem_7');
     sem_7.style.display = 'none';
+    
+    const sem1_2 = document.querySelector('.sem1_2');
+    sem1_2.style.backgroundColor = null;
+    const sem3 = document.querySelector('.sem3');
+    sem3.style.backgroundColor = 'black';
+    const sem4= document.querySelector('.sem4');
+    sem4.style.backgroundColor = null;
+    const sem5 = document.querySelector('.sem5');
+    sem5.style.backgroundColor = null;
+    const sem6 = document.querySelector('.sem6');
+    sem6.style.backgroundColor = null;
+    const sem7 = document.querySelector('.sem7');
+    sem7.style.backgroundColor = null;
 }
 function sem_4() {
     const sem_1_and_2 = document.querySelector('.sem_1_and_2');
@@ -65,6 +97,19 @@ function sem_4() {
     sem_6.style.display = 'none';
     const sem_7 = document.querySelector('.sem_7');
     sem_7.style.display = 'none';
+
+    const sem1_2 = document.querySelector('.sem1_2');
+    sem1_2.style.backgroundColor = null;
+    const sem3 = document.querySelector('.sem3');
+    sem3.style.backgroundColor = null;
+    const sem4= document.querySelector('.sem4');
+    sem4.style.backgroundColor = 'black';
+    const sem5 = document.querySelector('.sem5');
+    sem5.style.backgroundColor = null;
+    const sem6 = document.querySelector('.sem6');
+    sem6.style.backgroundColor = null;
+    const sem7 = document.querySelector('.sem7');
+    sem7.style.backgroundColor = null;
 }
 function sem_5() {
     const sem_1_and_2 = document.querySelector('.sem_1_and_2');
@@ -79,6 +124,19 @@ function sem_5() {
     sem_6.style.display = 'none';
     const sem_7 = document.querySelector('.sem_7');
     sem_7.style.display = 'none';
+
+    const sem1_2 = document.querySelector('.sem1_2');
+    sem1_2.style.backgroundColor = null;
+    const sem3 = document.querySelector('.sem3');
+    sem3.style.backgroundColor = null;
+    const sem4= document.querySelector('.sem4');
+    sem4.style.backgroundColor = null;
+    const sem5 = document.querySelector('.sem5');
+    sem5.style.backgroundColor = 'black';
+    const sem6 = document.querySelector('.sem6');
+    sem6.style.backgroundColor = null;
+    const sem7 = document.querySelector('.sem7');
+    sem7.style.backgroundColor = null;
 }
 function sem_6() {
     const sem_1_and_2 = document.querySelector('.sem_1_and_2');
@@ -93,6 +151,19 @@ function sem_6() {
     sem_6.style.display = 'flex';
     const sem_7 = document.querySelector('.sem_7');
     sem_7.style.display = 'none';
+
+    const sem1_2 = document.querySelector('.sem1_2');
+    sem1_2.style.backgroundColor = null;
+    const sem3 = document.querySelector('.sem3');
+    sem3.style.backgroundColor = null;
+    const sem4= document.querySelector('.sem4');
+    sem4.style.backgroundColor = null;
+    const sem5 = document.querySelector('.sem5');
+    sem5.style.backgroundColor = null;
+    const sem6 = document.querySelector('.sem6');
+    sem6.style.backgroundColor = 'black';
+    const sem7 = document.querySelector('.sem7');
+    sem7.style.backgroundColor = null;
 }
 function sem_7() {
     const sem_1_and_2 = document.querySelector('.sem_1_and_2');
@@ -107,6 +178,19 @@ function sem_7() {
     sem_6.style.display = 'none';
     const sem_7 = document.querySelector('.sem_7');
     sem_7.style.display = 'flex';
+
+    const sem1_2 = document.querySelector('.sem1_2');
+    sem1_2.style.backgroundColor = null;
+    const sem3 = document.querySelector('.sem3');
+    sem3.style.backgroundColor = null;
+    const sem4= document.querySelector('.sem4');
+    sem4.style.backgroundColor = null;
+    const sem5 = document.querySelector('.sem5');
+    sem5.style.backgroundColor = null;
+    const sem6 = document.querySelector('.sem6');
+    sem6.style.backgroundColor = null;
+    const sem7 = document.querySelector('.sem7');
+    sem7.style.backgroundColor = 'black';
 }
 function Go_back() {
     window.history.back(-1);
