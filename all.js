a50fffcb944d381e6c3df58e752484feb6f6bb55
Rushit16102004 <bodrarushit@gmail.com>
2024-04-06 18:38:02 +0530

    Add search functionality to error page; update HTML and CSS for error page; add media queries for smaller screen sizes; update CSS for navbar and dark mode button; remove oldpaper and studymaterial submenus from CSS.

--- all.js
+++ all.js
@@ -249,3 +249,43 @@ function darkModeON() {
     }
     premode = 'dark';
 }
+
+const resultsBox = document.querySelector(".result-box");
+    const inputBox = document.getElementById("input-box");
+  
+    const links = [
+      {
+        href: "www.example.com/faqs",
+        text: "Frequently asked questions",
+      },
+      {
+        href: "www.example.com/blog/article",
+        text: "Article Title",
+      },
+      {
+        href: "www.example.com/contact",
+        text: "Contact",
+      },
+    ];
+  
+    inputBox.onkeyup = function () {
+      let result = [];
+      let input = inputBox.value;
+      if (input.length) {
+        result = links.filter((link) => {
+          return link.text.toLowerCase().includes(input.toLowerCase());
+        });
+      }
+      display(result);
+    }
+    function display(result) {
+  if (result.length) {
+    const content = result.map((list, index) => {
+      const href = list.href;
+      return `<li><a href="${href}">${list.text}</a></li>`;
+    });
+    resultsBox.innerHTML = `<ul>${content.join('')}</ul>`;
+  } else {
+    resultsBox.innerHTML = '';
+  }
+}
